7fc351769d8a0e43ba22af02ceae299394274af5
Seaurchin*2 <psj22803540@gmail.com>
2021-09-09 00:21:56 +0900

    main file change, add champion name list

---
+++ main.py
@@ -0,0 +1,87 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+# print list of runes
+def printRune(list0):
+    print('[ ' + list0[0] + ' ]')
+    for i in range(1, len(list0)):
+        print(' ' + list0[i])
+
+
+champName = input('Enter champion\'s name: ')
+print()
+
+# main, rune, spell urls
+url = 'https://poro.gg/champions/' + champName + '/aram?hl=ko-KR'
+uuu = '#wrapper > div > div > div > div:nth-child(7) > div.row.row-small > '
+url_r = (uuu
+    + 'div.col-lg-8.col-xl-7 > div.champion-box.champion-box--rune-build '
+    + '> div.champion-box__content > div > div.champion-rune__content '
+    + '> div.champion-rune-tab-content.active.h-auto > div:nth-child(1)')
+
+url_s = (uuu
+    + 'div.col-lg-4.col-xl-5 > div > div.champion-box__content '
+    + '> div.champion-build.champion-build--spells > div.champion-build'
+    + '__content > div:nth-child(1) > div.champion-build__icons')
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    # basic settings
+    # --------------------------------------------------------------
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # open Rune page
+    # --------------------------------------------------------------
+    sample = soup.select_one(url_r)
+    runes_html = sample.find_all('img', {'class': 'active'})
+
+    rune_img_urls = [str(a.attrs['src']) for a in runes_html]
+
+    main_r = [str(a.attrs['alt']) for a in runes_html[:5]]  # primary rune
+    second_r = [str(a.attrs['alt']) for a in runes_html[5:8]]  # secondary rune
+    other_r = [str(a.attrs['alt']) for a in runes_html[8:]]  # adapt ability
+
+    # for item in rune_img_urls:
+    #    print(item)
+    # print('\n')
+
+    printRune(main_r)
+    print('-'*20)
+    printRune(second_r)
+    print('-'*20)
+    for item in other_r:
+        print(' ' + item)
+    print('\n')
+
+    # open Spell page
+    # --------------------------------------------------------------
+    sample = soup.select_one(url_s)
+    spells_html = sample.find_all('img')
+
+    spell_img_urls = [str(a.attrs['src']) for a in spells_html]
+    spells = [str(a.attrs['alt']) for a in spells_html]
+
+    # for item in spell_img_urls:
+    #    print(item)
+    print('\n', end='\n< ')
+
+    for item in spells:
+        print(item, end=' ')
+    print('>\n')
+
+    # download images
+    # --------------------------------------------------------------
+    for item in rune_img_urls:
+        download_url = url + item
+        img_name = item.replace('/', '')
+
+        print(item)
+        print(download_url)
+        print(img_name)
+        print('\n\n')
+        # urllib.request.urlretrieve(download_url, './img/' + img_name)
+else:
+    print(response.status_code + ' not found: Not correct champion name')
